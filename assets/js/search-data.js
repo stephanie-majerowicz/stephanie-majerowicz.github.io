c154fb1d9484178a82beb8e25f1bf973cff9b04a
christianresch <49751948+christianresch@users.noreply.github.com>
2026-04-26 21:48:26 +0000

    Deploying to gh-pages from @ stephanie-majerowicz/stephanie-majerowicz.github.io@3b57322dc0f0b2552746780f7624d97874e3f06c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -91,25 +91,11 @@ ninja.data = [{
           window.open("https://inspirehep.net/authors/1010907", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
