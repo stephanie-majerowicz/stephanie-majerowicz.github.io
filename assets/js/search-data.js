d10df783083180a77a3f1ed63b4805c41159c6be
christianresch <49751948+christianresch@users.noreply.github.com>
2026-04-26 22:41:10 +0000

    Deploying to gh-pages from @ stephanie-majerowicz/stephanie-majerowicz.github.io@e55108952f909b9d2f0da1c9951240c7a8cec0fd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -69,11 +69,6 @@ ninja.data = [{
           description: "This course covers the foundational aspects of impact evaluation.",
           section: "Teachings",handler: () => {
               window.location.href = "/teachings/impact-evaluation-2026-spring/";
-            },},{id: "teachings-introduction-to-machine-learning",
-          title: 'Introduction to Machine Learning',
-          description: "This course provides an introduction to machine learning concepts, algorithms, and applications. Students will learn about supervised and unsupervised learning, model evaluation, and practical implementations.",
-          section: "Teachings",handler: () => {
-              window.location.href = "/teachings/introduction-to-machine-learning/";
             },},{
         id: 'social-cv',
         title: 'CV',
